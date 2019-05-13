6496b0bb9a1660defdd43f12e95e711ad3f201ab
yumcoder-plusplus <yumcoder.platform@gmail.com>
2019-05-13 12:24:27 +0430

    setup antd with support custom theme :)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
 import React from 'react';
-import logo from './logo.svg';
+import {Form, Select, InputNumber, DatePicker, Switch, Slider, Button} from 'antd';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+const {Option} = Select;
+
+const App = () => (
+    <Form style={{marginTop: 32}}>
+        <Form.Item
+            label="Digital input box"
+            labelCol={{span: 8}}
+            wrapperCol={{span: 8}}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+            <InputNumber min={1} max={10} defaultValue={3}/>
+            <span className="ant-form-text"> Machine</span>
+            <a href="https://ant.design">Link text</a>
+        </Form.Item>
+        <Form.Item
+            label="switch"
+            labelCol={{span: 8}}
+            wrapperCol={{span: 8}}
+        >
+            <Switch defaultChecked/>
+        </Form.Item>
+        <Form.Item
+            label="Sliding input bar"
+            labelCol={{span: 8}}
+            wrapperCol={{span: 8}}
+        >
+            <Slider defaultValue={70}/>
+        </Form.Item>
+        <Form.Item
+            label="Selector"
+            labelCol={{span: 8}}
+            wrapperCol={{span: 8}}
+        >
+            <Select defaultValue="lucy" style={{width: 192}}>
+                <Option value="jack">jack</Option>
+                <Option value="lucy">lucy</Option>
+                <Option value="disabled" disabled>disabled</Option>
+                <Option value="yiminghe">yiminghe</Option>
+            </Select>
+        </Form.Item>
+        <Form.Item
+            label="Date selection box"
+            labelCol={{span: 8}}
+            wrapperCol={{span: 8}}
+        >
+            <DatePicker/>
+        </Form.Item>
+        <Form.Item wrapperCol={{span: 8, offset: 8}}>
+            <Button type="primary" htmlType="submit">
+                Submit
+            </Button>
+            <Button style={{marginLeft: 8}}>
+                Cancel
+            </Button>
+        </Form.Item>
+    </Form>
+);
 
 export default App;
